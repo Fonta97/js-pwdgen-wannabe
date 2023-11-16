441876a73a481adcba357ab7f42465202666ff27
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-16 16:28:48 +0100

    Inizio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// dichiaro variabili
+const userName = parseInt("Inserisci il tuo nome");
+const userSurname = prompt("Inserisci il tuo cognome");
+const favColor = prompt("Qual'è il tuo colore preferito?");
+
+console.log(userName, userSurname, favColor)
+
