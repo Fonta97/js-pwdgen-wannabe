49c6329cdc9e8894f085ba64db98b5202f3d388e
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-16 16:52:08 +0100

    push di mezzo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-// dichiaro variabili
-const userName = parseInt("Inserisci il tuo nome");
+// richiesta dati per creare pw
+const userName = prompt("Inserisci il tuo nome");
 const userSurname = prompt("Inserisci il tuo cognome");
 const favColor = prompt("Qual'è il tuo colore preferito?");
 
 console.log(userName, userSurname, favColor)
 
+let passwordSpeciale = userName + userSurname + favColor + 23;
+
+console.log(passwordSpeciale)
+
+document.getElementById("password_finale").innerHTML = "Ecco la tua password super sicura:" + passwordSpeciale
